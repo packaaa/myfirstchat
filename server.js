13ca36c53c421b3656acdd45cdcb245d6e8b97b8
听风来 <18106313276@163.com>
2017-09-11 21:58:23 +0800

    myChat

---
+++ server.js
@@ -0,0 +1,50 @@
+//服务器及页面部分
+var  express=require('express'),
+      app=express(),
+      server=require('http').createServer(app),
+     io=require('socket.io').listen(server),
+     users=[];
+     app.use('/',express.static(__dirname+'/www'));
+     server.listen(8080,()=>{console.log('running....')});
+     //socket部分
+     //登录
+     io.on('connection',(socket)=>{
+      //昵称设置
+      socket.on('login',(nickname)=>{
+        if (users.indexOf(nickname)>-1) {
+        sicket.emit('nickExisted');
+
+
+        }else{
+          socket.userIndex=users.length;
+          socket.nickname = nickname;
+          users.push(nickname);
+          socket.emit('loginSuccess');
+          io.sockets.emit('system', nickname, users.length, 'login'); //向所有连接到服务器的客户端发送当前登陆用户的昵称 
+
+        }
+
+
+      })
+      //断开连接的事件
+socket.on('disconnect', function() {
+    //将断开连接的用户从users中删除
+    users.splice(socket.userIndex, 1);
+    //通知除自己以外的所有人
+    socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+});
+//接收新消息
+    socket.on('postMsg', function(msg) {
+        //将消息发送到除自己外的所有用户
+        socket.broadcast.emit('newMsg', socket.nickname, msg);
+    });
+    //接收用户发来的图片
+ socket.on('img', function(imgData) {
+    //通过一个newImg事件分发到除自己外的每个用户
+     socket.broadcast.emit('newImg', socket.nickname, imgData);
+ });
+
+     })
+     
+  
+ 
